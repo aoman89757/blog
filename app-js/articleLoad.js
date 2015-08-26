3b0f74d8fb9e67ba400c7967b4b185bb4435cdb4
Taia <taia.feng@aquafadas.com>
2015-08-26 14:37:27 +0200

    Changed the file structure

---
+++ app-js/articleLoad.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){    
+        $("#article-title").load("../article-title/yosemite.txt");
+		$("#article-content").load("../article-content/yosemite.txt");
+});
---
+++ app-js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("foot01").innerHTML =
+    "<p>&copy;  " + new Date().getFullYear() + " Aoman89757. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+    "<ul id='menu'>" +
+        "<li><a href='html/index.html'>Home</a></li> " +
+        "<li><a href='html/Article.html'>Article</a></li> " +
+        "<li><a href='html/project.html'>Project</a></li> "+
+        "<li><a href='html/news.html'>News</a></li> "+
+        "<li><a href='html/about.html'>About me</a></li> "+
+    "</ul>"
